734bb38020f9d32c68aaba46c594018903c1195b
Ahmad Sheikh <ahmdsh747@gmail.com>
2023-08-22 23:45:43 +0500

    first commit

---
+++ code2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int hours = 0;
+    cout << endl;
+    cout << "How many credit hours do you have?";
+    cout << endl << "Answer:";
+    cin >> hours;
+
+    if (hours < 12)
+        for (int hours = 0; hours < 12;) {
+            cout << "You are ineligible to register" << endl << "Enter Again:";
+            cin >> hours;
+        }
+    cout << endl;
+    cout << "Congratulations, You are Registered." << endl;
+    return 0;
+}
